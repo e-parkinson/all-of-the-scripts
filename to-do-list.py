13561251b52a715cf7565277e700409cd889bc86
Esme Parkinson <e-parkinson@users.noreply.github.com>
2016-12-09 11:13:53 +0000

    Update to-do-list.py

--- to-do-list.py
+++ to-do-list.py
@@ -3,10 +3,22 @@
 
 def printLists():
     # print lists
-    
+    # this code needs tidying
+    # tabulate?
+    print('IN PROGRESS:)
+    for item in inprog_list:
+          print(item)
+    print('TO DO:')
+    for item in todo_list:
+        print(item)
+    print('RECENTLY COMPLETED:')
+          for item in compl_list:
+          print(item)
+     print(' ')
 def updateLists():
     # move items between lists
-
+    # if move makes length of completed items > 5, delete oldest (del compl_list[0])
+    
 # make lists to hold on to items
 todo_list = []
 inprog_list = []
@@ -14,7 +26,7 @@ compl_list = []
 
 # print out instructions on how to use the app
 helpInfo_str = 'Enter "SHOW" to display lists, "UPDATE" to move an item to another list, "HELP" to show this line and "DONE" when finished'
-print('Add an item to TODO list:')
+print('Add an item to TO DO list:')
 print(helpInfo_str)
 
 while True:
@@ -25,7 +37,7 @@ while True:
         break
     elif new_item == 'SHOW':
         printLists()
-        # give option to remove completed items
+
     elif new_item == 'HELP':
         print(helpInfo_str)
     elif new_item == 'UPDATE':
